ec439e3e313489440975c85c589579101a3d9407
yuxinqi <863733134@qq.com>
2020-12-25 14:03:53 +0800

    test

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,25 +1,26 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import { Home } from '@/views/Home'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import {Home} from '@/views/Home'
+import {About} from "@/views/About";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: <Home student={{ name: "string", class: "string", age: 5 }}></Home>
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: <Home student={{name: "string", class: "string", age: 5}}/>
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: <About/>
+    }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHistory(),
+    routes
 })
 
 export default router
---
+++ src/views/About.tsx
@@ -0,0 +1,10 @@
+import {defineComponent} from 'vue'
+
+export const About = defineComponent({
+    render() {
+        return (<div class="about">
+                <h1>This is an about page</h1>
+            </div>
+        )
+    }
+})
