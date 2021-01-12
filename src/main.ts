92c63e533fed6bfc4fccbb8cb01d4d4084cb97b0
yuxinqi <863733134@qq.com>
2021-01-12 13:50:54 +0800

    加入ant design vue

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { createApp } from 'vue'
-import { App } from './App'
-import router from './router'
+import App from './App.vue'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
-const app = createApp(App)
-// 分环境处理
-if (process.env.NODE_ENV === 'development') {
-    if ('__VUE_DEVTOOLS_GLOBAL_HOOK__' in window) {
-        // 这里__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue赋值一个createApp实例
-        (<any>window).__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = app
-    }
-}
-
-app.use(router).mount('#app')
+const app = createApp(App);
+app.use(Antd);
+app.mount('#app')
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,7 +1,5 @@
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue" {
+    import { ComponentOptions } from "vue";
+    const component: ComponentOptions
+    export default component;
 }
-
-declare module '*.png'
